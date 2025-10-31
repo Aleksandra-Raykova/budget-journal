707c99e734c1d6ed913798c31b81ecc878278f2a
Aleksandra-Raykova <a.d.raikova@gmail.com>
2025-10-31 18:00:41 +0200

    Set up skeleton pages (demo, privacy, terms, contact, login) and linked navigation

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # homepage
+    path('', views.home, name='homepage'),  # homepage
+    path('demo/', views.demo, name='demo'),  # demo journal page
+    path('privacy/', views.privacy, name='privacy'),  # privacy page
+    path('terms/', views.terms, name='terms'),  # terms page
+    path('contact/', views.contact, name='contact'),  # contacts page
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'homepage.html')
 
+def demo(request):
+    return render(request, 'demo.html')
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
+def terms(request):
+    return render(request, 'terms.html')
+
+def contact(request):
+    return render(request, 'contact.html')
