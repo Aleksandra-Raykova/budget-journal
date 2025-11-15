3e2e2b26353bdbaadc1e0df17a3de34620440aa9
Aleksandra-Raykova <a.d.raikova@gmail.com>
2025-11-15 13:38:12 +0200

    Add logout view and button

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, profile, ProfileLoginView
+from .views import RegisterView, profile, ProfileLoginView, ProfileLogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', ProfileLoginView.as_view(), name='login' ),
     path('profile/', profile, name='profile'),
+    path('logout/', ProfileLogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import FormView
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm, CustomUserLoginForm
 
 
@@ -16,6 +16,7 @@ class RegisterView(FormView):
         user = form.save()
         login(self.request, user)
         return super().form_valid(form)
+
 
 class ProfileLoginView(FormView):
     template_name = "login.html"
@@ -30,3 +31,10 @@ class ProfileLoginView(FormView):
 @login_required
 def profile(request):
     return render(request, 'profile.html')
+
+
+class ProfileLogoutView(View):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return redirect("homepage")
+
