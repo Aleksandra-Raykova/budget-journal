e1f58113ded3792a2c536fa8698c974ee1abf3e3
Aleksandra-Raykova <a.d.raikova@gmail.com>
2025-11-17 15:05:58 +0200

    Add delete user profile logic

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, profile, ProfileLoginView, ProfileLogoutView
+from .views import RegisterView, ProfileLoginView, ProfileLogoutView, ProfileDetailView, ProfileEditView, ProfileDeleteView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', ProfileLoginView.as_view(), name='login' ),
-    path('profile/', profile, name='profile'),
+    path('login/', ProfileLoginView.as_view(), name='login'),
+    path('profile/', ProfileDetailView.as_view(), name='profile'),
     path('logout/', ProfileLogoutView.as_view(), name='logout'),
+    path('profile/edit/', ProfileEditView.as_view(), name='edit_profile'),
+    path('profile/delete/', ProfileDeleteView.as_view(), name='delete_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView, UpdateView, DeleteView
 from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm, CustomUserLoginForm
+from .models import CustomUser
 
 
 class RegisterView(FormView):
@@ -28,13 +30,29 @@ class ProfileLoginView(FormView):
         return super().form_valid(form)
 
 
-@login_required
-def profile(request):
-    return render(request, 'profile.html')
-
-
 class ProfileLogoutView(View):
     def post(self, request, *args, **kwargs):
         logout(request)
         return redirect("homepage")
 
+
+class ProfileDetailView(LoginRequiredMixin, TemplateView):
+    template_name = "profile.html"
+
+
+class ProfileEditView(LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    fields = ["first_name", "last_name", "email"]
+    template_name = "edit_profile.html"
+    success_url = reverse_lazy("profile")
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class ProfileDeleteView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        logout(request)
+        user.delete()
+        return redirect("homepage")
